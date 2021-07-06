2370d6809aed61bce068604dc9ce27015ef96fac
Ricardo <ricardo.jucrist@hotmail.com>
2021-07-05 21:06:47 -0300

    styles

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,8 +1,9 @@
 import { Flex } from '@chakra-ui/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Navigation, Pagination} from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { SliderItem } from '../SliderItem';
+
 
 SwiperCore.use([Navigation, Pagination]);
 
@@ -16,28 +17,20 @@ interface SliderProps {
 }
 
 export const Slider = ({continents}: SliderProps) =>  {
-
-
   return (
     <Flex w="100%" h={["250px","450px"]} maxW="1240px" mx="auto" mb={["5","10"]}>
       <Swiper
-        wrapperTag="ul"
         spaceBetween={0}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         initialSlide={0}
-
       >
-
-        {continents.map(continent => {
-          console.log({continent})
-          return (
+        {continents.map(continent => (
           <SwiperSlide key={continent.slug}>
               <SliderItem continent={continent} />
           </SwiperSlide>
-        )})}
-
+        ))}
       </Swiper>
     </Flex>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import {AppProps} from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import NextNProgress from 'nextjs-progressbar';
+
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
 
 function MyApp({ Component, pageProps }:AppProps) {
   return (
   <ChakraProvider theme={theme}>
+     <NextNProgress
+        color="#FFBA08"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
    <Component {...pageProps} />
   </ChakraProvider>)
 }
